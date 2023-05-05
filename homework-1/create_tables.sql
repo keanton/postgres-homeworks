27eda81f79d2222d0a2ecb6e13ba17dd34ede1cf
Toha <you@example.com>
2023-05-06 00:27:34 +0300

    script

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE table employees
+(
+	emp_first_name varchar(100) NOT NULL,
+	emp_last_name varchar(100) NOT NULL,
+	emp_title varchar(100) NOT NULL,
+	emp_birtday varchar(30) NOT NULL,
+	notes varchar (999)
+);
+
+CREATE table orders
+(
+	order_id int PRIMARY KEY,
+	order_customer varchar(50) NOT NULL,
+	order_emp_id int NOT NULL,
+	order_date varchar(30) NOT NULL,
+	order_destination varchar(150) NOT NULL
+);
+
+CREATE table customers
+(
+    customer_id varchar(20) NOT NULL,
+	customer_name varchar(100) NOT NULL,
+	customer_contact varchar(50) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,80 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+class data_base:
+    def __init__(self, db_name, password, user='postgres'):
+        """
+        :param db_name: Название базы данных
+        :param user: Имя пользователя (администратора бд) (default = postgres)
+        :param password: Пароль
+        """
+        self.connection = psycopg2.connect(host='localhost', database=db_name,
+                              user=user, password=password)
+        self.cursor = self.connection.cursor()
+        self.connection.autocommit = True
+
+    def table_insert(self, from_file='', table_name=''):
+        """
+        :param from_file: Файл от куда берутся данные для бд,
+        расположение должно быть в папке north_data (example: customers_data.csv)
+        :param table_name: Название таблицы, куда будут вноситься данные
+
+        В ДАННЫЙ МОМЕНТ НЕ ИСПОЛЬЗУЕТСЯ
+        """
+
+        with open(f'north_data/{from_file}') as file:
+            csv_file = csv.reader(file)
+            next(csv_file)
+            for i in csv_file:
+                val = i[0], i[1], i[2]
+                if table_name.lower() == 'orders' or table_name == 'employees':
+                    print(f'Добавляем в таблицу {table_name} - {i}')
+                    self.cursor.execute(f'INSERT INTO {table_name.lower()} '
+                                        f'VALUES {i[0], i[1], i[2], i[3], i[4]}')
+                    self.connection.commit()
+                elif table_name.lower() == 'customers':
+                    print(f'Добавляем в таблицу Customers {i}')
+                    self.cursor.execute(f'INSERT INTO customers(customer_id, customer_name, customer_contact) '
+                                        f'VALUES(%s, %s, %s)', (val))
+                    self.connection.commit()
+                else:
+                    raise Exception(f'Такой таблицы не существует.')
+
+    #  Отдельные функции добавления
+    def table_insert_emp(self):
+        with open(f'north_data/employees_data.csv') as file:
+            csv_file = csv.reader(file)
+            next(csv_file)
+            for i in csv_file:
+                val = i[0], i[1], i[2], i[3], i[4]
+                self.cursor.execute(f'INSERT INTO employees(emp_first_name,emp_last_name,emp_title,emp_birtday,notes)'
+                                    f'VALUES(%s,%s,%s,%s,%s)', val)
+                self.connection.commit()
+
+    def table_insert_customers(self):
+        with open(f'north_data/customers_data.csv') as file:
+            csv_file = csv.reader(file)
+            next(csv_file)
+            for i in csv_file:
+                self.cursor.execute(f'INSERT INTO customers VALUES {i[0], i[1], i[2]}')
+                self.connection.commit()
+
+    def table_insert_orders(self):
+        with open(f'north_data/orders_data.csv') as file:
+            csv_file = csv.reader(file)
+            next(csv_file)
+            for i in csv_file:
+                val = i[0], i[1], i[2], i[3], i[4]
+                self.cursor.execute(f'INSERT INTO orders(order_id, order_customer, '
+                                    f'order_emp_id, order_date, order_destination '
+                                    f'VALUES(%s, %s, %s, %s, %s)', val)
+                self.connection.commit()
+
+
+if __name__ == '__main__':
+    db = data_base('north', '5772')
+    db.table_insert('customers_data.csv', 'customers')
+    db.table_insert('employees_data.csv', 'employees')
+    db.table_insert('orders_data.csv', 'orders')
